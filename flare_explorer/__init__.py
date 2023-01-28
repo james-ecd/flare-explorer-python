b4ad91c45b9c3c61ac0d4728885f2a4f876edaf7
James Davis <jamesecd@gmail.com>
2023-01-28 12:33:40 +0000

    Finish init imports

--- flare_explorer/__init__.py
+++ flare_explorer/__init__.py
@@ -1,4 +1,9 @@
-from .address import get_address
+# getter func imports for ease of use
+from .address import get_address, get_addresses
 from .block import get_block
 from .token_transfers import get_token_transfers
-from .transaction import get_transaction
+from .transaction import (
+    get_internal_transactions,
+    get_transaction,
+    get_transactions_from_address,
+)
